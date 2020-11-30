2ee0164611a6b223fefc2c04d186f96b383995d9
andreata <tasselli.a89@gmail.com>
2020-11-30 13:43:16 +0100

    add

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=73c4fad"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=73c4fad"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=73c4fad"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=73c4fad"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=73c4fad"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=cb8b4fb"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=cb8b4fb"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=cb8b4fb"
+    }
+  ]
+}
---
+++ src/.temp/now.js
@@ -0,0 +1,1 @@
+export default 1606735535233
